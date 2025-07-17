c24c834f600fba8ea7a610ba2decc3ac97b4e7bd
syedayaad01 <syedayaad01@mi.fu-berlin.de>
2025-07-17 09:53:33 +0200

    Updated config

--- lib/config.ts
+++ lib/config.ts
@@ -1,5 +1,5 @@
 const config = {
-    apiUrl: process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:5000',
+    apiUrl: process.env.NEXT_PUBLIC_BACKEND_API_URL || 'http://127.0.0.1:5000',
     supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL,
     supabaseAnonKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
 }
